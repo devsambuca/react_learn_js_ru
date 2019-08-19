9b9d1a2edb11e6a3bfdbad160b872b76d10ddc3d
devsambuca <stoyakovi4@gmail.com>
2019-08-19 18:36:37 +0300

    add articleList

---
+++ src/articleList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Article from "./article";
+
+export default function ArticleList({ articles }) {
+  const articleElement = articles.map(article => (
+    <li key={article.id}>
+      <Article article={article} />
+    </li>
+  ));
+  return <ul>{articleElement}</ul>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { render } from "react-dom";
-import Article from "./article";
 import { articles } from "./fixtures";
+import ArticleList from "./articleList";
 
-render(<Article article={articles[0]} />, document.getElementById("container"));
+render(
+  <ArticleList articles={articles} />,
+  document.getElementById("container")
+);
