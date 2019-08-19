34e4eeae0ef58d175f83d4ca2ae2f1f2bc46a0f9
devsambuca <stoyakovi4@gmail.com>
2019-08-19 21:22:25 +0300

    add commentList

---
+++ src/commentList.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import Comment from "./comment";
+
+export default class CommentList extends Component {
+  state = {
+    isOpen: true
+  };
+
+  render() {
+    const text = this.state.isOpen ? "hide comments" : "show comments";
+    return (
+      <div>
+        <button onClick={this.toggleOpen}>{text}</button>
+        {this.getBody()}
+      </div>
+    );
+  }
+
+  getBody() {
+    if (!this.state.isOpen) return null;
+    const { comments } = this.props;
+
+    if (!comments || !comments.length) return <p>No comments yet!</p>;
+
+    return (
+      <ul>
+        {comments.map(comment => (
+          <li key={comment.id}>
+            <Comment comment={comment} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  toggleOpen = ev =>
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+}
